99db42618929d1a4c522e9cf909ccb1e1140548a
phette23 <phette23@gmail.com>
2017-07-11 10:05:46 -0700

    home - mostly comments, small tweaks

--- libraries/home/models.py
+++ libraries/home/models.py
@@ -6,4 +6,15 @@ from wagtail.wagtailcore.models import Page
 
 
 class HomePage(Page):
+    # background_image = models.ForeignKey('wagtailimages.Image', ...)
+    # background_caption = RichTextField(blank=True)
+
+    # @TODO the blurbs for the 3 main sections (services, collections, about us)
+    # should be data represented in this model
+
+    # @TODO once models for subpages have been created, restrict the children here
+    # subpage_types = []
+
+    # don't allow more home pages to be created
+    parent_page_types = []
     pass
