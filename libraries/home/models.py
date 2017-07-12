f0dccd40e09d4011e4555af601d09e5a62d99966
phette23 <phette23@gmail.com>
2017-07-11 17:08:39 -0700

    home - add db fields for the 3 category descriptions

--- libraries/home/models.py
+++ libraries/home/models.py
@@ -3,17 +3,29 @@ from __future__ import absolute_import, unicode_literals
 from django.db import models
 
 from wagtail.wagtailcore.models import Page
+from wagtail.wagtailadmin.edit_handlers import FieldPanel, MultiFieldPanel
 
 
 class HomePage(Page):
     # background_image = models.ForeignKey('wagtailimages.Image', ...)
     # background_caption = RichTextField(blank=True)
 
-    # @TODO the blurbs for the 3 main sections (services, collections, about us)
-    # should be data represented in this model
+    # blurbs for the 3 main sections (services, collections, about us)
+    # we limit length & do not allow links like in a RichTextField
+    latin = 'Aliquam iaculis ornare tristique. Phasellus ullamcorper tristique est, ac dictum quam sagittis ut.'
+    services_text = models.CharField(default=latin, max_length=150)
+    collections_text = models.CharField(default=latin, max_length=150)
+    about_us_text = models.CharField(default=latin, max_length=150)
 
     subpage_types = ['categories.ServicesPage', 'categories.CollectionsPage', 'categories.AboutUsPage']
 
     # don't allow more home pages to be created
     parent_page_types = []
-    pass
+
+    content_panels = Page.content_panels + [
+        MultiFieldPanel([
+            FieldPanel('services_text'),
+            FieldPanel('collections_text'),
+            FieldPanel('about_us_text'),
+        ], heading="Category Descriptions")
+    ]
