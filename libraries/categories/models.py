e3534c1dd257f314c3215ebbf7dbe0e5fb49c9b2
phette23 <phette23@gmail.com>
2017-07-20 22:31:37 -0700

    HUGE revamp of model names

--- libraries/categories/models.py
+++ libraries/categories/models.py
@@ -46,25 +46,20 @@ class BaseStreamBlock(StreamBlock):
     snippet = RichTextBlock()
 
 
-class ServicesPage(Page):
+class CategoryPage(Page):
     parent_page_types = ['home.HomePage']
-    subpage_types = ['categories.ServicesRowPage']
+    subpage_types = ['categories.RowComponent']
 
-    # add child ServicesRowPage to context
+    # add child RowComponent to context
     def get_context(self, request):
-        context = super(ServicesPage, self).get_context(request)
+        context = super(CategoryPage, self).get_context(request)
         rows = self.get_children().live()
-        context['services_rows'] = rows
+        context['rows'] = rows
         return context
-
-    # allow only one instance of this page type
-    @classmethod
-    def can_create_at(cls, parent):
-        return super(ServicesPage, cls).can_create_at(parent) and not cls.objects.exists()
 
 
 class ServicePage(Page):
-    parent_page_types = ['categories.ServicesRowPage']
+    parent_page_types = ['categories.RowComponent']
     subpage_types = []
     # @TODO _here_ is the spot for some streamfields I think
     # reuse about_us_page template most likely
@@ -79,11 +74,11 @@ class ServicePage(Page):
 
 
 # does not have a matching template, should never be visited on its own
-# but only used as a component of ServicesPage
-# also since the ServicesRowPage is never directly rendered we can't use its
-# get_context() method to retrieve child services, that has to be done in template
-class ServicesRowPage(Page):
-    parent_page_types = ['categories.ServicesPage']
+# but only used as a component of a CategoryPage
+# also since the RowComponent is never directly rendered we can't use its
+# get_context() method to retrieve child pages, that has to be done in template
+class RowComponent(Page):
+    parent_page_types = ['categories.CategoryPage']
     subpage_types = ['categories.ServicePage']
     # this can't be RichTextField or the template screws up
     summary = models.CharField(max_length=350)
@@ -97,39 +92,35 @@ class ServicesRowPage(Page):
     ]
 
 
-class CollectionsPage(Page):
-    parent_page_types = ['home.HomePage']
-    # right now, no child collection pages, they all point external
-    # we'll almost certainly need to revisit this & make a subpage
-    # that can be referenced via the collection.link URLField
+class SpecialCollectionsPage(Page):
+    parent_page_types = ['categories.CategoryPage']
+    # right now, no child special collection pages, but could add in the future
     subpage_types = []
 
     # needs an orderable struct of some sort which contains a title, richtext blurb,
     # link to the external collection, and feature image _at least_
     content_panels = Page.content_panels + [
-        InlinePanel('collections', label='collection')
+        InlinePanel('special_collections', label='Special Collection')
     ]
 
-    # make collections page searchable by text of specific collections
+    # @TODO needs a main_image for search...maybe a method that returns
+    # the first image from a child SpecialCollection?
+
+    # make page searchable by text of child special collections
     search_fields = [
-        index.RelatedFields('collections', [
+        index.RelatedFields('special_collections', [
             index.SearchField('title'),
             index.SearchField('blurb'),
         ]),
     ]
 
-    # allow only one instance of this page type
-    @classmethod
-    def can_create_at(cls, parent):
-        return super(CollectionsPage, cls).can_create_at(parent) and not cls.objects.exists()
 
-
-class Collection(Orderable):
-    page = ParentalKey(CollectionsPage, related_name='collections')
+class SpecialCollection(Orderable):
+    page = ParentalKey(SpecialCollectionsPage, related_name='special_collections')
     title = models.CharField(max_length=255)
     # RichTextField allows links & text formatting so this is questionable
     blurb = RichTextField()
-    # URLField lets this link either internally or externally
+    # URLField lets this link be either internal or external
     link = models.URLField()
     image = models.ForeignKey(
         'wagtailimages.Image',
@@ -148,13 +139,8 @@ class Collection(Orderable):
 
 
 class AboutUsPage(Page):
-    parent_page_types = ['home.HomePage']
+    parent_page_types = ['categories.CategoryPage']
 
     # only fields are likely to be a featured image
     # and a stream field for text, pull quotes, images, etc.
     # similar to a blog post
-
-    # allow only one instance of this page type
-    @classmethod
-    def can_create_at(cls, parent):
-        return super(AboutUsPage, cls).can_create_at(parent) and not cls.objects.exists()
