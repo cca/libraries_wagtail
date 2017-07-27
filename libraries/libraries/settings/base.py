16291d927a6d0122b032cfa750278f95d064bf94
Mark Y. Goh <mgoh@cca.edu>
2017-07-27 11:59:34 -0700

    fixed a missing quote

--- libraries/libraries/settings/base.py
+++ libraries/libraries/settings/base.py
@@ -139,6 +139,6 @@ WAGTAIL_SITE_NAME = "CCA Libraries & Instructional Technology"
 
 # Base URL to use when referring to full URLs within the Wagtail admin backend -
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
-BASE_URL = 'http://libraries-dev.cca.edu
+BASE_URL = 'http://libraries-dev.cca.edu'
 
 INSTAGRAM_USERNAME = 'ccalibraries'
