ca78aeb1c5ec5c2925c8109320c833e5b65f6e06
Mark Y. Goh <mgoh@cca.edu>
2017-07-27 11:02:04 -0700

    removed DB and secret key

--- libraries/libraries/settings/base.py
+++ libraries/libraries/settings/base.py
@@ -11,14 +11,13 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 from __future__ import absolute_import, unicode_literals
+from .local_settings import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 
 PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 BASE_DIR = os.path.dirname(PROJECT_DIR)
-
-SECRET_KEY = 'm(zvr$pcz%5x*4qfpgs7*h#p(j15+cd&j^@ksb_^t2clblu5)^'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -28,7 +27,6 @@ ALLOWED_HOSTS = [
 	'127.0.0.1',
 	'libraries-dev.cca.edu',
 	'10.16.8.23',
-	'172.21.80.113',
 	]
 	
 # Application definition
@@ -101,23 +99,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'libraries.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'libraries_dev_cca_edu',
-        'USER': 'libuser',
-        'PASSWORD': 'SIidfjiw9x9Ghiai9weig',
-        'HOST': '10.16.8.25',#vm-postgres-04.cca.edu
-        },
-    'local': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
@@ -158,6 +139,6 @@ WAGTAIL_SITE_NAME = "CCA Libraries & Instructional Technology"
 
 # Base URL to use when referring to full URLs within the Wagtail admin backend -
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
-BASE_URL = 'http://example.com'
+BASE_URL = 'http://libraries-dev.cca.edu
 
 INSTAGRAM_USERNAME = 'ccalibraries'
