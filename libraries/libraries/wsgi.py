c1d03175a05ab8d3eec64afcf495f0fbcecd1ef5
Mark Y. Goh <mgoh@cca.edu>
2017-08-15 14:18:02 -0700

    changing the default setting file to use production parameters

--- libraries/libraries/wsgi.py
+++ libraries/libraries/wsgi.py
@@ -13,6 +13,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "libraries.settings.dev")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "libraries.settings.production")
 
 application = get_wsgi_application()
