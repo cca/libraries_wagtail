78cc02540ed3e0d3bd92588ba147b27ea3327f9a
phette23 <phette23@gmail.com>
2017-08-23 12:53:59 -0700

    sersol api - CORS header

--- libraries/sersol_api/views.py
+++ libraries/sersol_api/views.py
@@ -14,11 +14,17 @@ def sersol(request):
     else:
         # grab XML data from Sersol
         response = requests.get('http://ey7mr5fu9x.openurl.xml.serialssolutions.com/openurlxml?version=1.0&url_ver=Z39.88-2004&issn=%s' % issn)
+
+        # strip the ssopenurl: namespace prefixes
         namespaces = { 'http://xml.serialssolutions.com/ns/openurl/v1.0': None }
         # convert XML to python dict which will be in turn converted to JSON
         data = xmltodict.parse(
             response.text,
             process_namespaces=True,
             namespaces=namespaces,
-        )['openURLResponse']
-        return JsonResponse(data)
+        )['openURLResponse'] # everything's wrapped in this element
+
+        response = JsonResponse(data)
+        # let library catalog request this data
+        response['Access-Control-Allow-Origin'] = 'https://library.cca.edu'
+        return response
