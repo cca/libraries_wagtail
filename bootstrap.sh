e70a4ac1782564224b97202a0469a227e4e7cffe
phette23 <phette23@gmail.com>
2017-11-22 15:18:31 -0800

    bootstrap.sh - create cache table in db

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,22 @@
 #!/usr/bin/env bash
+
+# python & virtualenv setup
 virtualenv -p python3 .
 source bin/activate
 pip install -r libraries/requirements.txt
+
+# build frontend assets
 npm install
 npm run build
+
+# wagtail setup
 python libraries/manage.py migrate
 python libraries/manage.py createsuperuser
-python libraries/manage.py runserver
+python manage.py createcachetable
+
 # postgres setup
 createuser libuser
 createdb libraries_cca_edu --owner libuser
+
+# run dev server
+python libraries/manage.py runserver
