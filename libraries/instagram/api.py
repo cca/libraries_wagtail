4ff794fa98a36b0c9b70bb6318760712e6c7fcd9
phette23 <phette23@gmail.com>
2017-12-13 13:40:39 -0800

    insta - slightly better hashtag/username regex

--- libraries/instagram/api.py
+++ libraries/instagram/api.py
@@ -6,19 +6,21 @@ from django.conf import settings
 # @me -> <a href=link>@me</a> etc.
 def linkify_text(text):
     html = text
-    username_regex = r"@[a-zA-Z0-9]+"
-    hashtag_regex = r"#[a-zA-Z0-9]+"
+    username_regex = r"(^|\s)(@[a-zA-Z0-9]+)"
+    hashtag_regex = r"(^|\s)(#[a-zA-Z0-9]+)"
     ig_url = 'https://www.instagram.com/'
 
 
     def replace_username(match):
-        username = match.group(0).replace('@', '')
-        return '<a href="' + ig_url + username + '/">@' + username + '</a>'
+        leading_space = match.group(1)
+        username = match.group(2).replace('@', '')
+        return leading_space + '<a href="' + ig_url + username + '/">@' + username + '</a>'
 
 
     def replace_hashtag(match):
-        hashtag = match.group(0).replace('#', '')
-        return '<a href="' + ig_url + 'explore/tags/' + hashtag + '/">#' + hashtag + '</a>'
+        leading_space = match.group(1)
+        hashtag = match.group(2).replace('#', '')
+        return leading_space + '<a href="' + ig_url + 'explore/tags/' + hashtag + '/">#' + hashtag + '</a>'
 
     # re.sub(pattern, replacement, string)
     html = re.sub(username_regex, replace_username, html)
