6d5a870c3db3056a176a7f52ee570c9624396e98
phette23 <phette23@gmail.com>
2017-12-28 13:16:43 -0500

    Revert "never mind we don't need hours.js again lol"
    This reverts commit 343f84031329d1f9dd8cf401b79c6268b9fc7cb5.

---
+++ libraries/libraries/static/js/src/hours.js
@@ -0,0 +1,17 @@
+fetch('/hours/?format=json')
+    .then(response => response.json())
+    .then(data => {
+        let h = function (selector, datum='closed') {
+            let el = document.querySelector(selector)
+            if (el) el.innerText = datum
+        }
+
+        // info box hours
+        h('.home-info-box-times__timetable-times.js-simpson', data.Simpson)
+        h('.home-info-box-times__timetable-times.js-meyer', data.Meyer)
+        h('.home-info-box-times__timetable-times.js-materials', data.Materials)
+
+        // footer hours
+        h('.footer-section__site-address.js-meyer-hrs', data.Meyer + ' today')
+        h('.footer-section__site-address.js-simpson-hrs', data.Simpson + ' today')
+    })
