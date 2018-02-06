93f05ccd58afdbfdcc0f3f929776407bc741b258
phette23 <phette23@gmail.com>
2018-02-05 16:27:23 -0800

    brokenlinks - accept OPTIONS requests too

--- libraries/brokenlinks/views.py
+++ libraries/brokenlinks/views.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 import requests
 import json
@@ -10,6 +10,15 @@ def brokenlinks(request):
     if not settings.BROKENLINKS_GOOGLE_SHEET_KEY or not settings.BROKENLINKS_HASH:
         response = JsonResponse({"error": "This Wagtail app needs both a BROKENLINKS_GOOGLE_SHEET_KEY and BROKENLINKS_HASH in its settings to function."}, status=500)
         response['Access-Control-Allow-Origin'] = '*'
+        return response
+    # NOTE: some browser AJAX libraries send an "OPTIONS" request before POST
+    # so we have to handle this case, cannot only accept POST requests
+    elif request.method == 'OPTIONS':
+        response = HttpResponse()
+        response['Access-Control-Allow-Origin'] = '*'
+        response['Access-Control-Allow-Methods'] = 'POST, OPTIONS'
+        # note that '*' is not valid for Access-Control-Allow-Headers
+        response['Access-Control-Allow-Headers'] = 'origin, content-type, accept'
         return response
     elif request.method == 'POST':
         sheets_url = 'https://docs.google.com/a/cca.edu/forms/d/e/{0}/formResponse'.format(settings.BROKENLINKS_GOOGLE_SHEET_KEY)
