c8d2ee706ca2998ccdf6b52994318c8356128387
phette23 <phette23@gmail.com>
2018-02-05 16:51:01 -0800

    brokenlinks - finally fixed, JS is sending POST data not JSON

--- libraries/brokenlinks/views.py
+++ libraries/brokenlinks/views.py
@@ -22,14 +22,13 @@ def brokenlinks(request):
         return response
     elif request.method == 'POST':
         sheets_url = 'https://docs.google.com/a/cca.edu/forms/d/e/{0}/formResponse'.format(settings.BROKENLINKS_GOOGLE_SHEET_KEY)
-        body = json.loads(request.body)
         data = {
             settings.BROKENLINKS_HASH['ipaddress']: request.META.get('REMOTE_ADDR', ''),
-            settings.BROKENLINKS_HASH['openurl']: body.get('openurl', ''),
-            settings.BROKENLINKS_HASH['permalink']: body.get('permalink', ''),
-            settings.BROKENLINKS_HASH['type']: body.get('type', ''),
-            settings.BROKENLINKS_HASH['email']: body.get('email', ''),
-            settings.BROKENLINKS_HASH['comments']: body.get('comments', ''),
+            settings.BROKENLINKS_HASH['openurl']: request.POST.get('openurl', ''),
+            settings.BROKENLINKS_HASH['permalink']: request.POST.get('permalink', ''),
+            settings.BROKENLINKS_HASH['type']: request.POST.get('type', ''),
+            settings.BROKENLINKS_HASH['email']: request.POST.get('email', ''),
+            settings.BROKENLINKS_HASH['comments']: request.POST.get('comments', ''),
         }
         r = requests.post(sheets_url, data=data)
         response = JsonResponse(data, status=r.status_code)
