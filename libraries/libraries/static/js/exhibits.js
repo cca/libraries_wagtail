ed7bfc6a3ce29667b60b28081afa442b3296fc82
phette23 <phette23@gmail.com>
2018-08-13 17:34:58 -0700

    exhib - break JS out into file

---
+++ libraries/libraries/static/js/exhibits.js
@@ -0,0 +1,33 @@
+// exhibits app code, mostly lightGallery.js edits
+$(() => {
+    $('.js-masonry').masonry({
+        "columnWidth": 600,
+        "fitWidth": true,
+        "gutter": 4,
+        "itemSelector": ".js-gallery--work"
+    })
+
+    let options = {
+        hideBarsDelay: 3000,
+        selector: '.js-gallery--work',
+        subHtmlSelectorRelative: true,
+        vimeoPlayerParams: {
+            byline: false,
+            portrait: false,
+            title: false
+        }
+    }
+    let $lg = $('.js-lightgallery').lightGallery(options);
+
+    // only show zoom functions for images
+    $lg.on('onAfterAppendSubHtml.lg', (event, index) => {
+        let slide = $('.js-gallery--work').eq(index)
+        let zooms = $('#lg-zoom-in, #lg-zoom-out, #lg-actual-size')
+
+        if (slide.find('.gallery--work__image').length > 0) {
+            zooms.show()
+        } else {
+            zooms.hide()
+        }
+    })
+})
