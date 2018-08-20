9e514592efaa4b77676f6b6f40c8df265844cfde
phette23 <phette23@gmail.com>
2018-08-20 15:01:45 -0700

    exhib - fix admin JS show/hide of embed code

--- libraries/libraries/static/js/exhibits-admin.js
+++ libraries/libraries/static/js/exhibits-admin.js
@@ -5,17 +5,21 @@ $(function(){
 
     $('#id_exhibit_artwork-FORMS').change('.js-type', ev => {
         let select = $(ev.target)
-        let type = $(select).val()
-        let form = $(select).prevUntil('ul.fields')
+        let type = select.val()
+        let form = select.parents('ul.fields')
 
-        if (type == 'audio') {
+        if (type === 'audio') {
             form.find('.js-embed_code').hide()
+            // form.find('.js-media').show()
         } else if (type === 'html') {
             form.find('.js-embed_code').show()
+            // form.find('.js-media').hide()
         } else if (type === 'image') {
             form.find('.js-embed_code').hide()
+            // form.find('.js-media').hide()
         } else if (type === 'video') {
             form.find('.js-embed_code').hide()
+            // form.find('.js-media').show()
         }
     })
 })
