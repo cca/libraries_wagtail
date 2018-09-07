427e9f94044a75f40a4365a4bda41085ca91bbab
phette23 <phette23@gmail.com>
2018-09-07 16:47:48 -0700

    replicate more of the original document view

--- libraries/libraries/views.py
+++ libraries/libraries/views.py
@@ -1,6 +1,8 @@
-from django.http import HttpResponseRedirect
+from django.http import Http404, HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404
-from wagtail.documents.models import Document
+
+from wagtail.core import hooks
+from wagtail.documents.models import document_served, get_document_model
 
 
 def serve_wagtail_doc(request, document_id, document_filename):
@@ -9,5 +11,22 @@ def serve_wagtail_doc(request, document_id, document_filename):
     Wagtail's default document view serves everything as an attachment.
     We'll bounce back to the URL and let the media server serve it.
     """
+    # these first two passages copied from wagtail/documents/views/serve.py
+    Document = get_document_model()
     doc = get_object_or_404(Document, id=document_id)
+
+    # We want to ensure that the document filename provided in the URL matches
+    # the one associated with the considered document_id. If not we can't be
+    # sure that the document the user wants to access is the one corresponding
+    # to the <document_id, document_filename> pair.
+    if doc.filename != document_filename:
+        raise Http404('This document does not match the given filename.')
+
+    for fn in hooks.get_hooks('before_serve_document'):
+        result = fn(doc, request)
+        if isinstance(result, HttpResponse):
+            return result
+
+    # Send document_served signal
+    document_served.send(sender=Document, instance=doc, request=request)
     return HttpResponseRedirect(doc.file.url)
