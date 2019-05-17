a18f45d4cdb904fca214a38ffeeb9736be5d4895
phette23 <phette23@gmail.com>
2019-05-17 15:32:26 -0700

    home - one-time script to change owner of pages owned by etadmin

---
+++ libraries/home/management/commands/change_etadmin_owner.py
@@ -0,0 +1,23 @@
+import logging
+
+from django.contrib.auth import get_user_model
+from django.core.management.base import BaseCommand, CommandError
+from wagtail.core.models import Page
+
+logger = logging.getLogger('mgmt_cmd.script')
+
+
+class Command(BaseCommand):
+    help = 'Change the owner of pages owned by the ET Admin user.'
+
+    def add_arguments(self, parser):
+        parser.add_argument('username', nargs='?', type=str, help='username of new owner')
+
+    def handle(self, *args, **options):
+        User = get_user_model()
+        etadmin = User.objects.get(username='etadmin')
+        if options['username']:
+            new_owner = User.objects.get(username=options['username'])
+        else:
+            new_owner = User.objects.get(username='ephetteplace')
+        pages = Page.objects.filter(owner=etadmin).update(owner=new_owner)
