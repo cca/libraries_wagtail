6d01960ffcf2aa9011ef4165f3dcf2029ac56fa5
phette23 <phette23@gmail.com>
2019-05-17 15:43:15 -0700

    home - utility cmd to print pages w/o search_description

---
+++ libraries/home/management/commands/no_search_description.py
@@ -0,0 +1,11 @@
+from django.core.management.base import BaseCommand, CommandError
+from wagtail.core.models import Page, Site
+
+
+class Command(BaseCommand):
+    help = 'Print list of pages with no search_description field.'
+
+    def handle(self, *args, **options):
+        pages = Page.objects.live().filter(search_description='', depth__gte=2)
+        for page in pages:
+            print(page.title, Site.objects.first().hostname + page.url, page.owner)
