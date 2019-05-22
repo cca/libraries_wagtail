dacaded530d0a37fb8af741dc9132088e9a21da5
phette23 <phette23@gmail.com>
2019-05-22 12:07:20 -0700

    edits to schema.org data everywhere
    - remove <script> tags that can be in page.body
    - use `escapejs` filter everywhere so that quotes in text don't break JSON

--- libraries/home/templatetags/libraries_tags.py
+++ libraries/home/templatetags/libraries_tags.py
@@ -6,6 +6,7 @@ import re
 
 from django import template
 from django.template.defaultfilters import stringfilter
+from django.utils.safestring import mark_safe
 
 register = template.Library()
 
@@ -15,3 +16,13 @@ register = template.Library()
 def condense_whitespace(value):
     """ convert tabs & linebreaks into single spaces """
     return re.sub(r'\s+', ' ', re.sub(r'[\n\t]+', ' ', value) )
+
+@register.filter
+@stringfilter
+def stripjs(value):
+    """ strip <script> tags from HTML content
+    stackoverflow.com/questions/6444893/strip-javascript-code-before-rendering-in-django-templates
+    """
+    stripped = re.sub(r'<script(?:\s[^>]*)?(>(?:.(?!/script>))*</script>|/>)',
+                      '', value, flags=re.S)
+    return mark_safe(stripped)
