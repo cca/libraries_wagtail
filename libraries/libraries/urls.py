be6160a80a3e5268e60adac85135468d66643b5b
phette23 <phette23@gmail.com>
2019-05-28 12:50:34 -0700

    urls - rm unneeded comment

--- libraries/libraries/urls.py
+++ libraries/libraries/urls.py
@@ -49,10 +49,6 @@ urlpatterns = [
     # Wagtail's page serving mechanism. This should be the last pattern in
     # the list:
     url(r'', include(wagtail_urls)),
-
-    # Alternatively, if you want Wagtail pages to be served from a subpath
-    # of your site, rather than the site root:
-    #    url(r'^pages/', include(wagtail_urls)),
 ]
 
 
