610eabc3efa849f0e473ac5bf2db7cd3c38cfacd
phette23 <phette23@gmail.com>
2019-09-04 09:47:54 -0700

    search - support for exhibit page results, exhibits-only search

--- libraries/search/views.py
+++ libraries/search/views.py
@@ -7,9 +7,21 @@ from wagtail.core.models import Page
 from wagtail.search.models import Query
 
 from categories.models import RowComponent
+from exhibitions.models import ExhibitPage
 
 
 def search(request):
+    """
+    Handle HTTP search requests. Depending on the searchType GET parameter, a few
+    things could happen:
+        "all" -> redirect to Summon search
+        "services" -> query all Wagtail pages & render
+        "exhibits" -> query just Exhibit pages & render
+        "catalog" -> redirect to Koha search
+    Of these four options, neither catalog nor exhibits are actually exposed to
+    end users; they're just there in case we need them internally or want to add
+    this functionality later.
+    """
     search_query = request.GET.get('q', None)
     page = request.GET.get('page', 1)
 
@@ -23,16 +35,19 @@ def search(request):
         else:
             return redirect(summon_url)
     elif type == 'catalog':
-        koha_url = 'http://library.cca.edu/cgi-bin/koha/opac-search.pl?&q='
+        koha_url = 'https://library.cca.edu/cgi-bin/koha/opac-search.pl?&q='
         return redirect(koha_url + search_query, permanent=True)
 
     # Search
-    if search_query:
+    if type == 'services' and search_query:
         # exclude RowComponent pages
         search_results = Page.objects.not_type(RowComponent).live().search(search_query)
         query = Query.get(search_query)
-
         # Record hit
+        query.add_hit()
+    elif type == 'exhibits' and search_query:
+        search_results = Page.objects.type(ExhibitPage).live().search(search_query)
+        query = Query.get(search_query)
         query.add_hit()
     else:
         search_results = Page.objects.none()
