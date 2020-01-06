9e3c27511a571675d95a98ac053306eb56935f83
phette23 <phette23@gmail.com>
2020-01-06 13:10:09 -0800

    hours - validate JSON API parameters

--- libraries/hours/views.py
+++ libraries/hours/views.py
@@ -3,7 +3,7 @@ import datetime
 from django.shortcuts import redirect
 from django.views.decorators.cache import never_cache
 from django.http import JsonResponse
-from hours.models import get_open_hours, get_hours_for_lib, HoursPage
+from hours.models import get_open_hours, get_hours_for_lib, HoursPage, Library
 
 # hours API
 @never_cache
@@ -12,10 +12,15 @@ def hours(request):
         # can request hours for a given day, or library
         library = request.GET.get('library', None)
         date = request.GET.get('date', datetime.date.today())
+        success, error_response = validate(library, date)
+        if success is False:
+            return error_response
+
         hrs = None
 
         if library:
             hrs = get_hours_for_lib(library, date)
+            print(hrs)
 
             if hrs:
                 response = JsonResponse({
@@ -30,8 +35,8 @@ def hours(request):
         # we didn't find any hours via either of the above methods
         if hrs == None:
             response = JsonResponse({
-                "error": "no hours set found for library with name '%s' on date '%s'" % (library, date)
-            })
+                "error": "no hours set found for library with name '{}' on date '{}'".format(library, date)
+            }, status=404)
 
         response["Access-Control-Allow-Origin"] = "*"
         return response
@@ -39,3 +44,29 @@ def hours(request):
     # redirect HTML requests to the hours page
     else:
         return redirect(HoursPage.objects.first().url)
+
+
+def validate(library, date):
+    libraries = set(library.name for library in Library.objects.all())
+
+    # check that library is in our list
+    if library and library not in libraries:
+        response = JsonResponse({
+            "error": "Library '{}' does not exist. List of libraries: {}".format(library, libraries)
+        }, status=400)
+        response["Access-Control-Allow-Origin"] = "*"
+        return False, response
+
+    # if date string is provided must be in YYYY-MM-DD form
+    # check that date value is valid
+    if type(date) is str:
+        try:
+            datetime.datetime.strptime(date, '%Y-%m-%d')
+        except ValueError as err:
+            response = JsonResponse({
+                "error": "Unable to parse provided date string '{}'. ValueError: {}".format(date, err)
+            }, status=400)
+            response["Access-Control-Allow-Origin"] = "*"
+            return False, response
+
+    return True, None
