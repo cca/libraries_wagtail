395615ba0b51b3e5c9ff0dc901fb0cb8f36b49b2
phette23 <phette23@gmail.com>
2020-12-21 12:32:07 -0800

    wagsync script, consistent curly braces around bash vars

--- docs/wagsync.sh
+++ docs/wagsync.sh
@@ -2,15 +2,15 @@
 
 local_wagtail='/Users/ephetteplace/Code/libraries_wagtail'
 remote_wagtail='/opt/libraries_wagtail'
-static_destination="$local_wagtail/libraries/media"
-mkdir $static_destination 2>/dev/null
+static_destination="${local_wagtail}/libraries/media"
+mkdir ${static_destination} 2>/dev/null
 db='libraries_cca_edu'
 user='$user'
 dt=$(date "+%Y-%m-%d")
 pw='This is not the actual password :)'
 
 # NOTE: assumes an ssh alias of "live" for the production instance
-cd $static_destination
+cd ${static_destination}
 rsync -avz --progress --delete live:${remote_wagtail}/libraries/media/documents .
 rsync -avz --progress --delete live:${remote_wagtail}/libraries/media/original_images .
 rsync -avz --progress --delete live:${remote_wagtail}/libraries/media/images .
@@ -19,12 +19,12 @@ rsync -avz --progress --delete live:${remote_wagtail}/libraries/static/fonts lib
 
 # dump SQL db & download
 echo 'password is on your clipboard, paste at the prompt'
-echo $pw | pbcopy
+echo ${pw} | pbcopy
 ssh live "pg_dump -Fc -h vm-postgres-04.cca.edu -U $user -d ${db} > ${HOME}/${dt}.dump.sql"
 
-rsync -avz --progress live:~/$dt.dump.sql $static_destination
+rsync -avz --progress live:~/${dt}.dump.sql $static_destination
 
 # overwrite db, first time through we need to ensure user & db exist
-createuser $user 2>/dev/null
-createdb $db 2>/dev/null
-pg_restore --clean -h localhost -U $user -d ${db} ${static_destination}/${dt}.dump.sql
+createuser ${user} 2>/dev/null
+createdb ${db} 2>/dev/null
+pg_restore --clean -h localhost -U ${user} -d ${db} ${static_destination}/${dt}.dump.sql
