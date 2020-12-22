03f95ae3efc5fec8ff096c0092a8cf8ce228fb24
phette23 <phette23@gmail.com>
2020-12-22 09:22:23 -0800

    brokenlinks - document updates after last change

--- libraries/libraries/settings/brokenlinks.py
+++ libraries/libraries/settings/brokenlinks.py
@@ -3,9 +3,10 @@
 # This is just an example of what these settings look like.
 # You should _not_ use them—instead, copy these, put them in
 # your local.py & edit the keys to match your spreadsheet.
-# The key is in the URL like:
-# https://docs.google.com/forms/d/e/{{key}}/viewform
-# while the "entry.####" can be found by inspecting the form's <input> elements.
+# The URL is like:
+# https://docs.google.com/forms/d/e/{{key}}/formResponse
+# while the "entry.####" can be found by crearting a prefilled
+# response link & inspecting the URL query parameters.
 
 BROKENLINKS_GOOGLE_SHEET_KEY = '1FAIpQLSehVHSXLkZ5_gcAYxh5ZEktbU-0axbakVONq9lavfP1SXGc_A'
 BROKENLINKS_HASH = {
