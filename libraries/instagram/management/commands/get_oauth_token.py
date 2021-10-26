13f395b44d61b2c7155fd441b18d742c70633d48
Eric Phetteplace <phette23@gmail.com>
2021-10-26 13:41:13 -0700

    insta - fix attribute error in build
    AttributeError: 'NoneType' object has no attribute 'rstrip'
    caused by defaulting INSTA env var to None instead of empty string

--- libraries/instagram/management/commands/get_oauth_token.py
+++ libraries/instagram/management/commands/get_oauth_token.py
@@ -12,7 +12,7 @@ class Command(BaseCommand):
     help = "this will open an approval in a browser, then take you to a redirect URI with a 'code' parameter in the URL. It will use that code to obtain a shortlived access token and then exchange that in turn for a longlived access token."
 
     def handle(self, *args, **options):
-        if hasattr(settings, 'INSTAGRAM_APP_ID') and settings.INSTAGRAM_APP_ID is not None and hasattr(settings, 'INSTAGRAM_APP_SECRET') and settings.INSTAGRAM_APP_SECRET is not None and hasattr(settings, 'INSTAGRAM_REDIRECT_URI') and settings.INSTAGRAM_REDIRECT_URI is not None:
+        if hasattr(settings, 'INSTAGRAM_APP_ID') and settings.INSTAGRAM_APP_ID != '' and hasattr(settings, 'INSTAGRAM_APP_SECRET') and settings.INSTAGRAM_APP_SECRET != '' and hasattr(settings, 'INSTAGRAM_REDIRECT_URI') and settings.INSTAGRAM_REDIRECT_URI != '':
             self.stdout.write('Visit https://api.instagram.com/oauth/authorize?client_id={}&redirect_uri={}&scope=user_profile,user_media&response_type=code in a web browser, accept the prompt, then copy the code out of the URL that you are redirected to.'.format(settings.INSTAGRAM_APP_ID, settings.INSTAGRAM_REDIRECT_URI))
             code = input('Response code:')
             if get_token_from_code(code):
--- libraries/libraries/settings/base.py
+++ libraries/libraries/settings/base.py
@@ -375,8 +375,8 @@ BROKENLINKS_HASH = {
 }
 
 # Instagram app
-INSTAGRAM_APP_ID = env.get('INSTAGRAM_APP_ID', None).rstrip('\n')
-INSTAGRAM_APP_SECRET = env.get('INSTAGRAM_SECRET', None).rstrip('\n')
+INSTAGRAM_APP_ID = env.get('INSTAGRAM_APP_ID', '').rstrip('\n')
+INSTAGRAM_APP_SECRET = env.get('INSTAGRAM_SECRET', '').rstrip('\n')
 INSTAGRAM_REDIRECT_URI = 'https://libraries.cca.edu'
 
 # Summon app
