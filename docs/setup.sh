717e6ef504823b18682284a0e16ddb764b9b5035
phette23 <phette23@gmail.com>
2022-03-24 14:03:21 -0700

    add option to download db and/or media in setup script

--- docs/setup.sh
+++ docs/setup.sh
@@ -8,7 +8,8 @@ checkfor () {
 
 checkfor brew || {
     echo "Installing Homebrew..." && \
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" && \
+    /usr/local/bin/brew shellenv | source
 }
 checkfor fish || brew install fish
 checkfor docker || brew install --cask docker
@@ -22,4 +23,51 @@ minikube config set memory 8192
 # minikube config set disk-size 20000mb
 minikube addons enable metrics-server
 
-echo "Obtain a copy of secrets.env from a developer and place it in kubernetes/local"
+echo -e "\nObtain a copy of secrets.env from a developer and place it in kubernetes/local\n"
+
+echo "Do you want to load the (s)taging or (p)roduction database into your local minikube cluster now? You need gcloud installed and docker running but this shouldn't require a secrets.env file. Type \"n\" for \"no\". "
+read -r -n 1 -p "[n, p, s]? " result
+echo
+
+case ${result} in
+    "n")
+        echo "Skipping database download. You can run ./docs/sync.fish to download the database later."
+        ;;
+    "p")
+        minikube start
+        skaffold run -p db-only
+        ./docs/sync.fish --prod --db
+        ;;
+    "s")
+        minikube start
+        skaffold run -p db-only
+        ./docs/sync.fish --stage --db
+        ;;
+    *)
+        echo "Unrecognized input \"${result}\", skipping database download. You can run ./docs/sync.fish to download the database later."
+        ;;
+esac
+
+# shut down minikube if we started it above
+if not minikube status 2&>/dev/null; then
+    minikube stop
+fi
+
+echo -e "\nDo you want to load the (s)taging or (p)roduction media files onto your local machine? You need gcloud's gsutil installed but docker and minikube do not need to be running. Type \"n\" for \"no\". "
+read -r -n 1 -p "[n, p, s]? " result
+echo
+
+case ${result} in
+    "n")
+        echo "Skipping media download. You can run ./docs/sync.fish to download the media files later."
+        ;;
+    "p")
+        ./docs/sync.fish --prod --media
+        ;;
+    "s")
+        ./docs/sync.fish --stage --media
+        ;;
+    *)
+        echo "\nUnrecognized input \"$result\", skipping database download. You can run ./docs/sync.fish to download the media files later."
+        ;;
+esac
