313fde26d6982c6acab708dfcc30b51588ae4f5e
phette23 <phette23@gmail.com>
2022-03-24 17:36:38 -0700

    k8s - fix local init script
    `env` isn't on the image weirdly enough and our manage.py is in
    a different location from Portal

--- kubernetes/local/scripts/init.sh
+++ kubernetes/local/scripts/init.sh
@@ -1,8 +1,8 @@
-#!/usr/bin/env bash -e
+#!/bin/bash -e
 
 until psql -c '\l' -o /dev/null; do
   echo >&2 "$(date +%Y%m%dt%H%M%S) Postgres is unavailable - sleeping"
   sleep 1
 done
 
-python /app/manage.py runserver 0.0.0.0:8000
+python /app/libraries/manage.py runserver 0.0.0.0:8000
