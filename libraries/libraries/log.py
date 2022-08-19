629f8516d1c674eeb26f033881b856880c511b89
phette23 <phette23@gmail.com>
2022-08-19 12:05:54 -0700

    experiment with logging to console instead of files, ref #3
    no matter what happens we don't need/use the log files anymore
    though they're semi-useful, but the mgmt_cmd one for instance
    doesn't do anything because those are run in ephemeral cron job
    containers
    
    this also breaks logging settings out into a separate file

--- libraries/libraries/log.py
+++ libraries/libraries/log.py
@@ -7,21 +7,3 @@ class QuietDownElasticsearch(Filter):
         if record.name == 'elasticsearch' and record.levelname == 'INFO':
             return False
         return True
-
-
-class is404Error(Filter):
-    """ Log 404 Errors """
-    def filter(self,record):
-        if record.status_code == 404:
-            print(record.request.method)
-            return True
-        return False
-
-
-class isNot404Error(Filter):
-    """ Omit 404 Errors """
-    def filter(self,record):
-        if record.status_code != 404:
-            print(record.request.method)
-            return True
-        return False
