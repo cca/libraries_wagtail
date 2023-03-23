12317747de8ac5ad3d14a459ee994b2dc5dd2bf3
phette23 <phette23@gmail.com>
2023-03-23 16:10:58 -0700

    summon - add comment about GA4 & GTM relationship

--- libraries/libraries/static/summon/css-and-gtm.js
+++ libraries/libraries/static/summon/css-and-gtm.js
@@ -16,6 +16,7 @@ function ccaCustomizations() {
 
 function addGTM() {
     // https://github.com/tabathafarney/GoogleTagManager-Summon/blob/master/add-gtm-summon.js
+    // this GTM instance triggers Summon's GA4 property
     $("body").prepend("<script>var vPagePath = window.location.href;dataLayer = [{'virtualURL': vPagePath}];</script><noscript><iframe src='//www.googletagmanager.com/ns.html?id=GTM-TKFJGG7' height='0' width='0' style='display:none;visibility:hidden'></iframe></noscript><script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-TKFJGG7');</script>");
 }
 
