08a15b1d766fac3548eb35c00143042fcf064cee
phette23 <phette23@gmail.com>
2023-06-12 09:03:39 -0700

    delete analytics JS
    all it did was track file downloads but GA4 does that automatically

--- libraries/libraries/static/js/src/analytics.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll('a[href^="/documents/"]').forEach(a => {
-    a.addEventListener('click', ev => {
-        let u = new URL(a.href)
-          , path = u.pathname.split('/')
-          , id = path[2]
-          , name = path[3]
-        ga('send', 'event', 'download', id, name)
-    })
-});
