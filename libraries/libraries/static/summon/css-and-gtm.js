b0bb99ac5125f17b104ef536303070a62878a1cc
phette23 <phette23@gmail.com>
2023-11-10 16:28:25 -0800

    summon: fix js error related to logo img

--- libraries/libraries/static/summon/css-and-gtm.js
+++ libraries/libraries/static/summon/css-and-gtm.js
@@ -1,8 +1,8 @@
 (function() {
 
 function ccaCustomizations() {
-    const d = document
-    const css = d.createElement('link')
+    let d = document
+    let css = d.createElement('link')
     css.setAttribute('rel', 'stylesheet')
     css.setAttribute('type', 'text/css')
     css.href = 'https://libraries.cca.edu/static/summon/summon.css'
@@ -10,12 +10,17 @@ function ccaCustomizations() {
     d.querySelector('head').append(css)
 
     // adjust main logo size/spacing on large screens
-    d.querySelector('div[size="large"] .logo')
-        .setAttribute('src', 'https://libapps.s3.amazonaws.com/sites/2210/banner/libguide-logo-fa17.png')
+    const logo = d.querySelector('div[size="large"] .logo')
+    if (logo) logo.setAttribute('src', 'https://libapps.s3.amazonaws.com/sites/2210/banner/libguide-logo-fa17.png')
 }
 
-ccaCustomizations()
-// defined in broken-link-modal.js
-brokenLinkReports()
+function main() {
+    ccaCustomizations()
+    // defined in broken-link-modal.js
+    brokenLinkReports()
+}
+
+// logo is loaded asynchronously, so wait for it to appear
+setTimeout(main, 500)
 
 })();
