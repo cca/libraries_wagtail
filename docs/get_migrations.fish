f65cecc8d295db8f51148d19caddc5287e62a64b
phette23 <phette23@gmail.com>
2024-02-21 13:31:04 -0800

    fix: POD var in get_migrations script

--- docs/get_migrations.fish
+++ docs/get_migrations.fish
@@ -1,7 +1,8 @@
 #!/usr/bin/env fish
 echo "Syncing migrations files from minikube pod to host machine..."
 set NS libraries-wagtail
-set POD (kubectl -n$NS get pods -o jsonpath='{.items[0].metadata.name}' | grep wagtail | head -n1)
+set POD (kubectl -n$NS get pods -o jsonpath='{.items[*].metadata.name}' | tr ' ' '\n' | grep wagtail | head -n1)
 for dir in (find . -type d -name migrations)
-    kubectl -n$NS cp $POD:(string replace './' '/app/' $dir) $dir
+    set dest $POD:(string replace './' /app/ $dir)
+    kubectl -n$NS cp $dest $dir
 end
