ff4181900cfbc3eb0534b6987459edd041b832a2
phette23 <phette23@gmail.com>
2024-02-26 13:02:59 -0800

    fix: add libs wiki to new Wag 4.1 Help menu

--- libraries/home/wagtail_hooks.py
+++ libraries/home/wagtail_hooks.py
@@ -6,25 +6,34 @@ from wagtail.admin.menu import MenuItem
 from .views import NoSearchDescriptionReport
 
 
-# add a "help" section to admin menu that links out to our Google wiki
+# add our wiki to the "help" section of the main admin menu
 # doesn't belong under home but hooks don't work in main "libraries" directory
-@hooks.register('register_admin_menu_item')
-def register_frank_menu_item():
+@hooks.register("register_help_menu_item")
+def register_libraries_wiki_menu_item():
     return MenuItem(
-        'Help',
-        'https://sites.google.com/cca.edu/librarieswiki/home/websites-servers/editing-in-wagtail',
-        classnames='icon icon-help',
-        order=10000,
+        "Libraries Wiki",
+        "https://sites.google.com/cca.edu/librarieswiki/home/websites-servers/editing-in-wagtail",
+        classnames="icon icon-help",
+        order=0,
     )
 
 
-@hooks.register('register_reports_menu_item')
+@hooks.register("register_reports_menu_item")
 def register_no_search_desc_report_menu_item():
-    return MenuItem("Pages lacking Search Description", reverse('no_search_description_report'), classnames='icon icon-' + NoSearchDescriptionReport.header_icon, order=700)
+    return MenuItem(
+        "Pages lacking Search Description",
+        reverse("no_search_description_report"),
+        classnames="icon icon-" + NoSearchDescriptionReport.header_icon,
+        order=700,
+    )
 
 
-@hooks.register('register_admin_urls')
+@hooks.register("register_admin_urls")
 def register_no_search_desc_report_url():
     return [
-        path('reports/no-search-description/', NoSearchDescriptionReport.as_view(), name='no_search_description_report'),
+        path(
+            "reports/no-search-description/",
+            NoSearchDescriptionReport.as_view(),
+            name="no_search_description_report",
+        ),
     ]
