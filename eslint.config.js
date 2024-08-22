726da3eb848604ce80d17378f3b190fc4e03df44
phette23 <phette23@gmail.com>
2024-08-22 10:51:29 -0700

    feat: eslint for JS files

---
+++ eslint.config.js
@@ -0,0 +1,40 @@
+const globals = require('globals')
+const js = require("@eslint/js")
+
+module.exports = [
+    js.configs.recommended,
+    // "global" ignores must be alone in a config object
+    // https://eslint.org/docs/latest/use/configure/configuration-files#globally-ignoring-files-with-ignores
+    {ignores: ["**/*.min.js"]},
+    // client-side js for the website & summon
+    {
+        files: ["libraries/**/*.js"],
+        languageOptions: {
+            ecmaVersion: 2022,
+            globals: {
+                ...globals.browser,
+                angular: 'readonly',
+                jQuery: 'readonly',
+                $: 'readonly',
+            },
+            sourceType: "script"
+        },
+        name: "wagtail browser js",
+        rules: {
+            indent: [ 'warn', 4 ],
+            'linebreak-style': [ 'error', 'unix' ],
+        }
+    },
+    // scripts run via node
+    {
+        files: ["eslint.config.js", "gulpfile.js"],
+        languageOptions: {
+            ecmaVersion: 2022,
+            globals: {
+                ...globals.commonjs,
+            },
+            sourceType: "commonjs"
+        },
+        name: "repo node js",
+    },
+]
