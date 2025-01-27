d6dc8b057e6d8b485600a6389dcc352e57e34e0d
phette23 <phette23@gmail.com>
2025-01-27 11:48:03 -0800

    feat: allow SVG image uploads

--- libraries/libraries/settings/base.py
+++ libraries/libraries/settings/base.py
@@ -180,6 +180,9 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
+# Allow SVG uploads
+WAGTAILIMAGES_EXTENSIONS: list[str] = ["gif", "jpg", "jpeg", "png", "webp", "svg"]
+
 # Static files (CSS, JavaScript, Images)
 STATICFILES_FINDERS = [
     "django.contrib.staticfiles.finders.FileSystemFinder",
