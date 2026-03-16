dc116078ccaeba62b0674ac4145b6b794cfcffa0
phette23 <phette23@gmail.com>
2026-03-16 11:59:59 -0700

    style: silence misleading type warning
    I don't know why VS code warns but the xmltodict docs say
    you can map a XMLNS to None to disable parsing it & it works

--- libraries/sersol_api/views.py
+++ libraries/sersol_api/views.py
@@ -24,7 +24,7 @@ def sersol(request):
         data = xmltodict.parse(
             response.text,
             process_namespaces=True,
-            namespaces=namespaces,
+            namespaces=namespaces,  # type: ignore
         )["openURLResponse"]  # everything's wrapped in this element
 
         response = JsonResponse(data)
