10a32715ea99cc240d67d0f055563efce1bda116
phette23 <phette23@gmail.com>
2026-07-22 15:18:48 -0700

    test: don't run brokenlinks test w/o local server

--- libraries/brokenlinks/tests/tests.py
+++ libraries/brokenlinks/tests/tests.py
@@ -1,3 +1,5 @@
+import socket
+import warnings
 from unittest import TestCase
 
 import requests
@@ -5,9 +7,20 @@ import requests
 
 # Create your tests here.
 class TestBrokenLink(TestCase):
-    def test_post_broken_link(self):
+    def test_post_broken_link(self) -> None:
         """POST data to the brokenlinks app (local server must be running)"""
-        data = {
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+            sock.settimeout(1)
+            is_running: bool = sock.connect_ex(("127.0.0.1", 8000)) == 0
+
+        if not is_running:
+            warnings.warn(
+                "Skipping broken link POST test: local server is not running on 127.0.0.1:8000",
+                RuntimeWarning,
+            )
+            self.skipTest("Local server is not running on 127.0.0.1:8000")
+
+        data: dict[str, str] = {
             "ipaddress": "127.0.0.1",
             "openurl": "http://example.com/?super=long&query=string",
             "permalink": "http://example.com/1234",
@@ -15,5 +28,5 @@ class TestBrokenLink(TestCase):
             "email": "me@me.com",
             "comments": "help me please",
         }
-        r = requests.post("http://127.0.0.1:8000/brokenlinks/", data=data)
+        r = requests.post("http://127.0.0.1:8000/brokenlinks/", data=data, timeout=5)
         self.assertEqual(r.status_code, 200)
