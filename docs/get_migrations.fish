057f3fa4c344bf1a7a7ceeb539343281d48d1b52
phette23 <phette23@gmail.com>
2026-07-31 12:01:41 -0700

    fix: find migrations dirs only in local app
    the find command was going through the .venv before and checking way more folders than necessary

--- docs/get_migrations.fish
+++ docs/get_migrations.fish
@@ -2,7 +2,7 @@
 echo "Syncing migrations files from minikube pod to host machine..."
 set NS libraries-wagtail
 set POD (kubectl -n$NS get pods -o jsonpath='{.items[*].metadata.name}' | tr ' ' '\n' | grep wagtail | head -n1)
-for dir in (find . -type d -name migrations)
+for dir in (find libraries -type d -name migrations)
     set dest $POD:(string replace './' /app/ $dir)
     kubectl -n$NS cp $dest $dir
 end
